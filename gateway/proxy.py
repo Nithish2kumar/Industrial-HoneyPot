9d8914ab0d6b7d08fdfa0ca0ca74a941002dfd4b
Nithish2Kumar <jnithishkumar2@gmail.com>
2026-07-16 23:27:23 +0530

    Gateway connects client to PLC

--- gateway/proxy.py
+++ gateway/proxy.py
@@ -10,7 +10,12 @@ def startProxy():
     print(f"Gateway is listening on {host}:{port}")
     while True:
         clientSocket, clientAddress=server.accept()
+        plcSocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        plcSocket.connect(("127.0.0.1",502))
+        print("Connected to Real PLC")
         print(f"Client Connected: {clientAddress}")
+        
+        
 
 if __name__=="__main__":
     startProxy()
