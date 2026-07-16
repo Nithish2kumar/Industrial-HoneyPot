308ca71c71008faeb83933e054e11ceefa4a4be3
Nithish2Kumar <jnithishkumar2@gmail.com>
2026-07-16 23:12:07 +0530

    Implemented gateway TCP listener

--- gateway/proxy.py
+++ gateway/proxy.py
@@ -4,7 +4,13 @@ host="0.0.0.0"
 port=5020
 
 def startProxy():
-    pass
+    server=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((host, port))
+    server.listen(5)
+    print(f"Gateway is listening on {host}:{port}")
+    while True:
+        clientSocket, clientAddress=server.accept()
+        print(f"Client Connected: {clientAddress}")
 
 if __name__=="__main__":
     startProxy()
