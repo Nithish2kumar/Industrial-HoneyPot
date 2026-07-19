3ca16d327fe90b134a0acb9ce24c5048c84c0169
Nithish2Kumar <jnithishkumar2@gmail.com>
2026-07-19 15:18:26 +0530

    Created a separate method to handle the client's request.

--- gateway/proxy.py
+++ gateway/proxy.py
@@ -10,26 +10,29 @@ def startProxy():
     server.listen(5)
     print(f"Gateway is listening on {host}:{port}")
     while True:
-        clientSocket, clientAddress=server.accept()
+        clientSocket, clientAddress = server.accept()
         print(f"Client Connected: {clientAddress}")
-        plcSocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        plcSocket.connect(("127.0.0.1",502))
+        plcSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        plcSocket.connect(("127.0.0.1", 502))
         print("Connected to Real PLC")
-        while True:
-            request=clientSocket.recv(1024)
-            if not request:
-                print("Client Disconnected")
-                break
-            plcSocket.sendall(request)
-            response=plcSocket.recv(1024)
-            res=parse(request)
-            print(res)
-            clientSocket.sendall(response)
-        
-        clientSocket.close()
-        plcSocket.close()
-        print("Connection Closed")
-        
+        handleRequest(clientSocket, plcSocket)
+
+
+def handleRequest(clientSocket,plcSocket):
+    while True:
+        request = clientSocket.recv(1024)
+        if not request:
+            print("Client Disconnected")
+            break
+        plcSocket.sendall(request)
+        response = plcSocket.recv(1024)
+        res = parse(request)
+        print(res)
+        clientSocket.sendall(response)
+
+    clientSocket.close()
+    plcSocket.close()
+    print("Connection Closed")
 
 if __name__=="__main__":
     startProxy()
