beebb1e8fc21f77d345d59710eea3f9f44e0040e
Nithish2Kumar <jnithishkumar2@gmail.com>
2026-07-20 23:41:46 +0530

    Edited detector

--- gateway/detector.py
+++ gateway/detector.py
@@ -2,6 +2,8 @@
 def detect(parse):
     packet=parse
     print(packet)
-    if packet["function_code"]==6:
+    restrictedReg=[0,3]
+    if packet["function_code"]==6 and packet["address"] in restrictedReg:
+        print("ALERT: Attempt to modify restricted register.")
         return "DROP"
     return "OK"
