e75f97ff6a3db08843e26cd0e31f4f472ec271c3
Nithish2Kumar <jnithishkumar2@gmail.com>
2026-07-20 23:55:39 +0530

    Fixed the issue

--- gateway/risk.py
+++ gateway/risk.py
@@ -3,11 +3,11 @@ restrictedReg=[0,3]
 
 def calculate(packet):
     global risk
-    if packet["funciton_code"]==3:
+    if packet["function_code"]==3:
         risk+=0
-    elif packet["funciton_code"]==6:
+    elif packet["function_code"]==6:
         risk+=20
-    elif packet["funciton_code"]==6 and packet["address"] in restricedReg:
+    elif packet["function_code"]==6 and packet["address"] in restrictedReg:
         risk+=40
     elif packet["function_code"]!=3 or 6:
         risk+=50
