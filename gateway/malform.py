ec2d974f497155dfd25544b82f22b63a6d1372e7
Nithish2Kumar <jnithishkumar2@gmail.com>
2026-07-28 23:28:07 +0530

    Fixed issue

--- gateway/malform.py
+++ gateway/malform.py
@@ -1,12 +1,10 @@
-address=[1,2,3,4]
+address=[0,1,2,3,4]
 
 def malformFN(res):
-    if len(res)<12:
+
+    if res["protocol_id"] !=0:
         return "MALFORM"
-    elif res["protocol_id"] !=0:
-        return "MALFORM"
-    elif res["length"]!=len(res)-6:
-        return "MALFORM"
+
     elif res["function_code"]==3 and res["address"] not in address:
         return  "MALFORM"
     elif res["function_code"]==6 and res["address"] not in address and not res["value"]:
